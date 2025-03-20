9d62c0be8e1805f489ba8a3a6ed06d7bb64cb70f
Sercan TAN <sercan.tan@khas.edu.tr>
2025-03-21 01:39:54 +0300

    Change URL Scheme

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        URL::forceScheme('https');
+        
         Inertia::share([
             'flash' => function () {
                 return [
